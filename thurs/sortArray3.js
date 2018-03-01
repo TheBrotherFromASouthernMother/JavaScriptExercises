e6ba51047b42f7bea87a87f993753df9269515fc
Christian Lowe <christglowe@gmail.com>
2018-03-01 11:52:17 -0600

    finished array sort exercsie 3

--- thurs/sortArray3.js
+++ thurs/sortArray3.js
@@ -1,10 +1,26 @@
+
+var arr = [
+  [1, 3, 4],
+  [2, 4, 6, 8],
+  [3, 6]
+];
+
+//function to feed into reduce (I think this acts as a closure)
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
 function sortArray(array) {
   sortedArrays = array.sort(function(curr, next) {
-    if (curr.length < next.length ) {
-      return 1;
-    }
-    if (curr.length > next.length) {
+
+    //reduces the values within each array into a single value
+    curr = curr.reduce(reducer)
+    next = next.reduce(reducer)
+
+    //compares the values of the arrays and sorts them based on inequality
+    if (curr < next ) {
       return -1;
+    }
+    if (curr > next) {
+      return 1;
     }
     return 0;
   })
@@ -12,3 +28,5 @@ function sortArray(array) {
   console.log(sortedArrays)
   return sortedArrays
 }
+
+sortArray(arr)
