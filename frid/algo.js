efad235ec4ce0157643b44a36f7a9e8aeb5a69ad
Christian Lowe <christglowe@gmail.com>
2018-03-03 14:55:14 -0600

    Got halfway through BlackJack game and got bored

---
+++ frid/algo.js
@@ -0,0 +1,45 @@
+// function beerBottles(number) {
+//   var myBeer = ""
+//   for (var i = number; i > 0; i --) {
+//     myBeer = "Beer"
+//     if (i % 35 === 0) {
+//       myBeer = "Miller-Lite"
+//     } else if (i % 7 === 0) {
+//       myBeer = "Milller"
+//     } else if (i % 5 === 0) {
+//       myBeer = "Light Beer"
+//     }
+//     console.log(`${i} bottles of ${myBeer} on the wall ${i} bottles of ${myBeer}, take one down pass it around ${i} bottles of ${myBeer}!`)
+//   }
+// }
+//
+//
+// beerBottles(50)
+
+
+
+// let myBeer = '';
+//
+// for (i = 99; i > 0; i--) {
+//     let isValid = true;
+//
+//
+//     switch(isValid) {
+//         case(i % 35 === 0):
+//             myBeer = 'Miller Lite';
+//             break;
+//
+//         case(i % 7 === 0):
+//             myBeer = 'Miller';
+//             break;
+//
+//         case(i % 5 === 0):
+//             myBeer = 'Lite';
+//             break;
+//
+//          default:
+//             myBeer = 'Beer'
+//
+//     }
+//     console.log(`${i} bottles of ${myBeer} on the wall, take one down, pass it around, ${i - 1} bottles of ${myBeer} on the wall.`);
+// }
